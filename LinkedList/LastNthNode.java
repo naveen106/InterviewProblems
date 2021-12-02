19e4df8572b37b7516f6c141039967d718ee9bdb
Naveen <nk0187912@gmail.com>
2021-12-02 20:02:37 +0530

    changed directory name

---
+++ LinkedList/LastNthNode.java
@@ -0,0 +1,45 @@
+package InterviewProblems;
+
+import LinkedList.LinkedListNode;
+import LinkedList.MakeLinkedList;
+
+import java.util.Scanner;
+
+/*
+Ninja has been given a Singly Linked List having N nodes.
+As part of a test, he has been asked to find the Nth node from the end of the linked list.
+Since Ninja is not so good at studies, he asked you to help him with the same.
+
+  Your task is to find the Nth node from last.
+  For Example
+  If the given list is (4 -> 3 -> 2 -> 7) and N = 2:
+  Then the 2nd node from the end is 2.
+*/
+public class LastNthNode {
+    static LinkedListNode<Integer>findLastNth(LinkedListNode<Integer> head, int n){
+      LinkedListNode<Integer> pointer1 = head, pointer2 = head;
+      while(n!=0){
+        pointer2 = ((LinkedListNode<Integer>) pointer2).next;
+        n--;
+      }
+      while(pointer2.next != null){
+        pointer1 = pointer1.next;
+        pointer2 = pointer2.next;
+      }
+      return pointer1;
+    }
+  public static void main(String[] args) {
+    LinkedListNode<Integer> head = new MakeLinkedList().MakeLinkedList();
+
+    Scanner input = new Scanner(System.in);
+    System.out.println("Enter the last n-th node position: ");
+
+    head = findLastNth(head, input.nextInt());
+    input.close();
+
+    while(head != null){
+      System.out.print(head.data + " ");
+      head = head.next;
+    }
+  }
+}
